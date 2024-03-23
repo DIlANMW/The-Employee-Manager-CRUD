50b61813f8c899cddc5995b7dbd05d72d0c682c3
DILANMW <dilanweerasinghe@icloud.com>
2024-03-23 09:59:56 +0530

    feat: Add database connection

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
+import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 
+import dbConnection from "./utils/dbConnection.js";
+
+dotenv.config();
 const app = express();
 app.use(cors());
+dbConnection();
 app.use(express.json());
 
 app.listen(3000, () => {
